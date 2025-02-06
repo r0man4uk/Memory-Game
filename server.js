1f2a6928fb3a1907080d4ee39b04110245765370
Yuriy Romanchuk <romanchuk.yura@gmail.com>
2025-02-06 10:46:30 +0200

    server port 8080 on railways

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const DATA_FILE = path.join(__dirname, 'scores.json');
 
 app.use(express.json());
