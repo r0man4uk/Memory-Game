36a8c5296e976e7f84fdd29bb8ff2e59bd1a9679
Yuriy Romanchuk <romanchuk.yura@gmail.com>
2025-02-07 14:14:43 +0200

    add env.PPORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DATA_FILE = path.join(__dirname, 'scores.json');
 
 app.use(express.json());
